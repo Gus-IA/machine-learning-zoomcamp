e0bd735d9570e717e4e8b53f226631564bd2375c
Gus-IA <garciagus1992@gmail.com>
2026-05-07 09:33:58 +0200

    Dataset and Dataloader

--- pytorch_ml_zoomcamp08.py
+++ pytorch_ml_zoomcamp08.py
@@ -77,3 +77,82 @@ top5_classes = [categories[i] for i in top5_indices]
 print("Top 5 predictions:")
 for i, class_name in enumerate(top5_classes):
     print(f"{i+1}: {class_name}")
+
+
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+
+
+# Custom dataset class
+class ClothingDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+        self.classes = sorted(os.listdir(data_dir))
+        self.class_to_idx = {cls: i for i, cls in enumerate(self.classes)}
+
+        for label_name in self.classes:
+            label_dir = os.path.join(data_dir, label_name)
+            for img_name in os.listdir(label_dir):
+                self.image_paths.append(os.path.join(label_dir, img_name))
+                self.labels.append(self.class_to_idx[label_name])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+from torchvision import transforms
+
+# Preprocessing
+
+input_size = 224
+
+# ImageNet normalization values
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+# Simple transforms - just resize and normalize
+train_transforms = transforms.Compose(
+    [
+        transforms.Resize((input_size, input_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+    ]
+)
+
+val_transforms = transforms.Compose(
+    [
+        transforms.Resize((input_size, input_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+    ]
+)
+
+
+from torch.utils.data import DataLoader
+
+# Dataloader
+
+train_dataset = ClothingDataset(
+    data_dir="./clothing-dataset-small/train", transform=train_transforms
+)
+
+val_dataset = ClothingDataset(
+    data_dir="./clothing-dataset-small/validation", transform=val_transforms
+)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
