86eac2047463dce1a9114e64f453e90d4295eec7
Gus-IA <garciagus1992@gmail.com>
2026-09-23 10:32:05 +0200

    Data analysis

--- update/car_price_project.py
+++ update/car_price_project.py
@@ -19,3 +19,33 @@ print(df.head())
 # tipo de columna
 strings = list(df.dtypes[df.dtypes == "object"])
 print(strings)
+
+# valores únicos por columna
+for col in df.columns:
+    print(col)
+    print(df[col].unique()[:5])
+    print(df[col].nunique())
+    print()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# distribución de precios menores a 100.000
+sns.histplot(df.MSRP[df.MSRP < 100000], bins=50)
+plt.show()
+
+import numpy as np
+
+# logaritmo
+print(np.log([1, 10, 1000, 100000]))
+
+# lo aplicamos
+price_logs = np.log1p(df.MSRP)
+print(price_logs)
+
+# lo visualizamos
+sns.histplot(price_logs, bins=50)
+plt.show()
+
+# missing values
+print(df.isnull().sum())
